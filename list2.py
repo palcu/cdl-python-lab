0d65ee72e73669a407c6f488d26385b387eda963
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-09-02 22:33:13 +0300

    Refactor list exercises

--- list2.py
+++ list2.py
@@ -1,20 +1,23 @@
-#!/usr/bin/python -tt
-# Copyright 2010 Google Inc.
-# Licensed under the Apache License, Version 2.0
-# http://www.apache.org/licenses/LICENSE-2.0
+from tester import test
 
-# Google's Python Class
-# http://code.google.com/edu/languages/google-python-class/
 
-# Additional basic list exercises
-
-# D. Given a list of numbers, return a list where
-# all adjacent == elements have been reduced to a single element,
-# so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
-# modify the passed in list.
-def remove_adjacent(nums):
+# D. sort_last
+# Given a list of non-empty tuples, return a list sorted in increasing
+# order by the last element in each tuple.
+# e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
+# [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
+# Hint: use a custom key= function to extract the last element form each tuple.
+def sort_last(tuples):
   # +++your code here+++
   return
+
+
+test(sort_last([(1, 3), (3, 2), (2, 1)]),
+     [(2, 1), (3, 2), (1, 3)])
+test(sort_last([(2, 3), (1, 2), (3, 1)]),
+     [(3, 1), (1, 2), (2, 3)])
+test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
+     [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
 
 
 # E. Given two lists sorted in increasing order, create and return a merged
@@ -25,41 +28,10 @@ def linear_merge(list1, list2):
   # +++your code here+++
   return
 
-# Note: the solution above is kind of cute, but unforunately list.pop(0)
-# is not constant time with the standard python list implementation, so
-# the above is not strictly linear time.
-# An alternate approach uses pop(-1) to remove the endmost elements
-# from each list, building a solution list which is backwards.
-# Then use reversed() to put the result back in the correct order. That
-# solution works in linear time, but is more ugly.
 
-
-# Simple provided test() function used in main() to print
-# what each function returns vs. what it's supposed to return.
-def test(got, expected):
-  if got == expected:
-    prefix = ' OK '
-  else:
-    prefix = '  X '
-  print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
-
-
-# Calls the above functions with interesting inputs.
-def main():
-  print('remove_adjacent')
-  test(remove_adjacent([1, 2, 2, 3]), [1, 2, 3])
-  test(remove_adjacent([2, 2, 3, 3, 3]), [2, 3])
-  test(remove_adjacent([]), [])
-
-  print()
-  print('linear_merge')
-  test(linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc']),
-       ['aa', 'bb', 'cc', 'xx', 'zz'])
-  test(linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']),
-       ['aa', 'bb', 'cc', 'xx', 'zz'])
-  test(linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb']),
-       ['aa', 'aa', 'aa', 'bb', 'bb'])
-
-
-if __name__ == '__main__':
-  main()
+test(linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc']),
+     ['aa', 'bb', 'cc', 'xx', 'zz'])
+test(linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']),
+     ['aa', 'bb', 'cc', 'xx', 'zz'])
+test(linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb']),
+     ['aa', 'aa', 'aa', 'bb', 'bb'])
